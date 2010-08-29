11c567a4d41dca1e33dc1b92e88bce120dc4a79b
Mark John Buenconsejo <mjwork@simpleteq.com>
2010-08-29 15:56:53 +0800

    added support for enforcing proper coding of POST values

--- lib/palmade/camping_ext/mixins.rb
+++ lib/palmade/camping_ext/mixins.rb
@@ -7,5 +7,6 @@ module Palmade::CampingExt
     autoload :DynamicRoutes, File.join(CAMPING_EXT_LIB_DIR, 'camping_ext/mixins/dynamic_routes')
     autoload :BetterMiddleware, File.join(CAMPING_EXT_LIB_DIR, 'camping_ext/mixins/better_middleware')
     autoload :ExceptionHandling, File.join(CAMPING_EXT_LIB_DIR, 'camping_ext/mixins/exception_handling')
+    autoload :CharsetEncoding, File.join(CAMPING_EXT_LIB_DIR, 'camping_ext/mixins/charset_encoding')
   end
 end
---
+++ lib/palmade/camping_ext/mixins/charset_encoding.rb
@@ -0,0 +1,43 @@
+module Palmade::CampingExt
+  module Mixins
+    module CharsetEncoding
+      DEFAULT_OPTIONS = { :charset_encoding => 'BINARY' }
+
+      class CharsetEncodingEnforcer
+        def initialize(app, camping, options = { })
+          @options = DEFAULT_OPTIONS.merge(options)
+
+          @app = app
+          @camping = camping
+        end
+
+        def call(env)
+          @app.call(enforce_encoding(env))
+        end
+
+        def enforce_encoding(env)
+          r = Rack::Request.new(env)
+          enc = Encoding.find(r.content_charset || @options['charset_encoding'])
+
+          unless enc.nil?
+            form_hash = r.POST
+
+            # note, only values are being enforced for now. it looks
+            # like Rack::Request#POST freezes the keys.
+            form_hash.values.each { |v| v.force_encoding(enc) if v.respond_to?(:force_encoding) }
+          end
+
+          env
+        end
+
+        def change_encoding
+
+        end
+      end
+
+      def self.included(base)
+        base.use CharsetEncodingEnforcer, base
+      end
+    end
+  end
+end
